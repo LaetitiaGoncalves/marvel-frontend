902b6c00ebf5fb4d1d9f747146bd1b6a42814d4f
Laetitia Goncalves <laetitiagoncalves@macbook-pro-de-laetitia.home>
2022-08-03 22:07:02 +0200

    resolve mistakes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,23 +9,26 @@ const Home = () => {
     try {
       const fetchDatas = async () => {
         const response = await axios.get(
-          "https://laetitia-marvel-project.herokuapp.com/"
+          "https://laetitia-marvel-project.herokuapp.com/comics"
         );
-        console.log(response.data);
         setData(response.data);
-        setIsLoading(false);
+        setIsLoading(true);
       };
       fetchDatas();
     } catch (error) {
       console.log(error.message);
     }
-  });
+  }, []);
   return isLoading === true ? (
     <div>En cours de chargement</div>
   ) : (
     <div className="homePage">
       <h1>Marvel</h1>
-      <p>{data}</p>
+      <div>
+        {data.results.map((comics, index) => {
+          return <li key={index}>{comics.title}</li>;
+        })}
+      </div>
     </div>
   );
 };
